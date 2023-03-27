ad3ab3a4b42f2c0753955080834b1a34d95ab831
ainurkamal <ainurkamaleev@gmail.com>
2023-03-27 08:46:15 +0300

    Add type hintigs

--- blog/models.py
+++ blog/models.py
@@ -1,27 +1,28 @@
 from django.db import models
 from django.shortcuts import reverse
+from typing import Type
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True)
-    body = models.TextField(blank=True, db_index=True)
-    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
-    date_pub = models.DateTimeField(auto_now_add=True)
+    title: str = models.CharField(max_length=150, db_index=True)
+    slug: str = models.SlugField(max_length=150, unique=True)
+    body: str = models.TextField(blank=True, db_index=True)
+    tags: Type['Tag'] = models.ManyToManyField('Tag', blank=True, related_name='posts')
+    date_pub: models.DateTimeField = models.DateTimeField(auto_now_add=True)
 
-    def get_absolute_url(self):
+    def get_absolute_url(self: 'Post') -> str:
         return reverse('post_detail_url', kwargs={'slug': self.slug})
 
-    def __str__(self):
+    def __str__(self: 'Post') -> str:
         return f'{self.title}'
 
 
 class Tag(models.Model):
-    title = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=50, unique=True)
+    title: str = models.CharField(max_length=50)
+    slug: str = models.SlugField(max_length=50, unique=True)
 
-    def get_absolute_url(self):
+    def get_absolute_url(self: 'Tag') -> str:
         return reverse('tag_detail_url', kwargs={'slug': self.slug})
 
-    def __str__(self):
+    def __str__(self: 'Tag') -> str:
         return f'{self.title}'
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,24 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from .models import Post, Tag
+from typing import List
 
 
-def posts_list(request):
-    posts = Post.objects.all()
+def posts_list(request: HttpRequest) -> HttpResponse:
+    posts: List[Post] = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
+def post_detail(request: HttpRequest, slug: str) -> HttpResponse:
+    post: Post = Post.objects.get(slug__iexact=slug)
     return render(request, 'blog/post_detail.html', context={'post': post})
 
 
-def tags_list(request):
-    tags = Tag.objects.all()
+def tags_list(request: HttpRequest) -> HttpResponse:
+    tags: List[Tag] = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
+def tag_detail(request: HttpRequest, slug: str) -> HttpResponse:
+    tag: Tag = Tag.objects.get(slug__iexact=slug)
     return render(request, 'blog/tag_detail.html', context={'tag': tag})
