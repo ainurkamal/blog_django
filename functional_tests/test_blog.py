db5554db2bd7757622972dd27ef76aa53ae04137
ainurkamal <ainurkamaleev@gmail.com>
2023-03-27 10:51:19 +0300

    Button "Read" test is OK

--- functional_tests/test_blog.py
+++ functional_tests/test_blog.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 try:
@@ -8,9 +9,15 @@ try:
     browser = webdriver.Chrome()
     browser.get(url)
     assert 'Posts list' in browser.title
-
 except AssertionError as error:
-    print("Home page test failed: ", error)
+    print("Home page test failed\n", error)
+
+try:
+    button = browser.find_element(By.CSS_SELECTOR, "a.btn")
+    time.sleep(1)
+    button.click()
+except NoSuchElementException as error:
+    print("Button test failed\n", error)
 
 finally:
     time.sleep(5)
