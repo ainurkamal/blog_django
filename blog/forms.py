a81445f0a5667c00a45da0208c0f85b3c5fd86d5
ainurkamal <ainurkamaleev@gmail.com>
2023-03-29 09:20:04 +0300

    Create static/css/styles.css
    * Add input placelder to TagForm

--- blog/forms.py
+++ blog/forms.py
@@ -10,14 +10,18 @@ class TagForm(forms.ModelForm):
         fields: List[str] = ['title', 'slug']
 
         widgets: dict = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'slug': forms.TextInput(attrs={'class': 'form-control'})
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Input title'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Input slug'})
         }
 
     def clean_slug(self: 'TagForm') -> str:
         new_slug: str = self.cleaned_data['slug'].lower()
+
         if new_slug == 'create':
             raise forms.ValidationError('Slug may not be "Create"')
+
         if Tag.objects.filter(slug__iexact=new_slug).count():
-            raise forms.ValidationError(f'Slug must be unique. "{new_slug}" slug already exists')
+            raise forms.ValidationError(
+                f'Slug must be unique. "{new_slug}" slug already exists')
+
         return new_slug
--- functional_tests/test_posts.py
+++ functional_tests/test_posts.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 
-try: # Posts page title name
+try:  # Posts page title name
     url = "http://127.0.0.1:5000/blog/"
     browser = webdriver.Chrome()
     browser.get(url)
@@ -12,14 +12,14 @@ try: # Posts page title name
 except AssertionError as error:
     print("Posts page name title test failed\n", error)
 
-try: # Click on Read some post
+try:  # Click on Read some post
     read_button = browser.find_element(By.CSS_SELECTOR, "a.btn")
     time.sleep(1)
     read_button.click()
 except NoSuchElementException as error:
     print("Click on Posts page Read button test failed\n", error)
 
-try: # Return to Posts page
+try:  # Return to Posts page
     nav_posts_button = browser.find_element(By.CSS_SELECTOR, "a.nav-posts")
     time.sleep(1)
     nav_posts_button.click()
