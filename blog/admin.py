cb17e98ea42feea8594cb4538c095c4f1b2361bd
ainurkamal <ainurkamaleev@gmail.com>
2023-04-14 15:42:44 +0300

    Upgrade admin panel
    * Russified admin panel
    * Added columns to Post and Tag models
    * Maked migration

--- blog/admin.py
+++ blog/admin.py
@@ -4,10 +4,19 @@ from .models import Post, Tag
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'slug', 'date_pub', 'tags_list')
+    list_display_links = ('title', 'slug')
+    search_fields = ('title', 'body')
+
+    def tags_list(self, obj):
+        return ", ".join([tag.title for tag in obj.tags.all()])
+    
+    tags_list.short_description = "Теги"
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'slug')
+    list_display_links = ('title', 'slug')
+    search_fields = ('title',)
 
